28fbfef9ce1fd00ed857f4975089bddaef3d0eb4
spuddie1984 <spuddie1984@hotmail.com>
2019-08-12 11:24:38 +0800

    1st commit Installation and Setup - Refer to README.md for more info

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # RESTful-recipe-book-app
 A RESTful Recipe App to collect my favourite recipes so i can view them for later use - Based on Colt Steele's Web Developer Bootcamp
+
+## RESTful Routes
+
+| name  |       url        | verb  |            Decription                   |
+|-------|------------------|-------|-----------------------------------------|
+|INDEX  | /recipes         | GET   | Displays a list of all recipes          |
+|NEW    | /recipes/new     | GET   | Displays a form to add a new recipe     |
+|CREATE | /recipes         | POST  | Add a new recipe to the DB              |
+|SHOW   | /recipes/:id     | GET   | Show info about one recipe only         |
+|EDIT   | /recipes/:id/edit| GET   | Show edit form for one recipe           |
+|UPDATE | /recipes/:id     | PUT   | Update a recipe, then redirect somewhere|
+|DESTROY| /recipes/:id     | DELETE| Delete a recipe, then redirect somewhere|
+ 
+## Commit Overview
+
+### 1st Commit - Installation and Setup
+- Install Express, mongoose, handlebars, foundation and dotenv 
+- Setup Express, mongoose 
+- Create schema for DB
+- create basic folder directory structure  
+- create basic routes
---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express'),
+      mongoose = require('mongoose'), 
+      app = express();
+
+// For Environment Variables(to be stored in .env)
+require('dotenv').config();
+// set our local port
+const PORT = process.env.LOCAL_PORT;
+
+///////////// SETTINGS /////////////
+// set view engine template to ejs
+app.set('view engine', 'ejs');
+// body parser for POST requests
+app.use(express.urlencoded({ extended: true }));
+// setup static includes folder
+app.use(express.static(__dirname + "/includes" ));
+
+////////// DATABASE SETUP //////////
+mongoose.connect('mongodb://localhost/recipes_app',{ useNewUrlParser: true });
+
+/////// DB SCHEMA AND MODEL ///////
+
+const Schema = mongoose.Schema;
+// Define the campgroundSchema keys and key types
+const recipeSchema = new Schema({
+   title: String,
+   catergory: [String],
+   image: String,
+   url: String,
+   ingredients: {
+       subList: {
+           subIngredients: String,
+       },
+       ingredients: String,
+   },
+   method: [String],
+});
+// set the database model
+const Recipes = mongoose.model('Recipes',recipeSchema);
+
+// ROUTES
+
+// ROOT
+// Redirect to INDEX ROUTE
+app.get('/', (req, res) => {
+    res.redirect('/recipes')
+});
+
+// INDEX
+// Show all Recipes
+app.get('/recipes', (req, res) => {
+    res.render('index');
+});
+
+// NEW 
+// Create a new recipe form
+app.get('/recipes/new', (req, res) => {
+    res.send('Create a new recipe form');
+});
+
+// CREATE
+// This adds the recipe to the database and normally redirects somewhere
+app.post('/recipes', (req, res) => {
+    res.send('This adds the recipe to the database and normally redirects somewhere');
+});
+
+// SHOW
+// This displays one recipe only
+app.get('/recipes/:id', (req, res) => {
+    res.send('This displays one recipe only');
+});
+
+// EDIT
+// This shows an edit form for one previously existing recipe
+app.get('/recipes/:id/edit', (req, res) => {
+    res.send('This shows an edit form for one previously existing recipe');
+});
+
+// UPDATE
+// This updates the recipe in the DB then redirects somewhere
+app.put('/recipes/:id', (req, res) => {
+    res.send('This updates the recipe in the DB then redirects somewhere');
+});
+
+// DESTROY
+// This deletes a recipe then redirects somewhere
+app.delete('/recipes/:id', (req, res) => {
+    res.send('This deletes a recipe then redirects somewhere');
+});
+
+// Our server is listening for requests on the specified port
+app.listen(PORT, () => {
+    console.log(`Listening ${PORT}!`);
+});
